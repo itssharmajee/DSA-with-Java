f669c83e206024caf48bc8e997ebb5017edd5eee
itssharmajee <sgautamkumar71@gmail.com>
2024-06-30 20:44:53 +0530

    added new file

---
+++ Demo3.java
@@ -0,0 +1,19 @@
+public class Demo3 {
+
+    public static void add(int data){
+        
+    }
+    public static void main(String[] args) {
+        
+    }
+}
+
+class Node{
+    int data;
+    Node next;
+
+    public void Node(int data){
+        this.data = data;
+        this.next = null;
+    }
+}
